546c48e4832fd7d58abbee2e27592608ce475bae
fvargas93 <vargasm.f93@gmail.com>
2024-05-09 18:22:30 -0400

    Saludo inicial y solicitud de nombre de usuario e intentos

---
+++ cachipun.py
@@ -0,0 +1,12 @@
+print('#'*27)
+print('¡Bienvenido al Cachipún! 😎')
+print('#'*27)
+username = input('\nIngresa tu nombre de usuario: ')
+
+while len(username) < 3:
+    username = input('El nombre de usuario debe tener al menos 3 carácteres ')
+    
+attempts = int(input('\n¿Cuantos intentos quieres jugar?: '))
+
+
+
